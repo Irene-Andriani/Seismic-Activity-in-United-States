0cdf58466c9e913ad3bc75b6203f94df91c28fde
Irene-Andriani <ireneandriani396@gmail.com>
2025-12-17 11:24:14 +0700

    Initialize Git repo and push app.py to GitHub
    Initialized Git repository and pushed app.py to remote.

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+cd
+ls
+app.py
 
+git init
+git add app.py
+git commit -m "initial commit with app.py"
+git branch -M main
+git remote add origin https://github.com/Irene-Andriani/Seismic-Activity-in-United-States.git
+git push -u origin main
